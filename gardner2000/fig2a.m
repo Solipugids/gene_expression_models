54bd0bcf2f188210c7f91bc0fca476407fc85a49
WANG Chao <caedmon.wang@gmail.com>
2020-11-05 16:25:35 +0800

    gardner2000 Fig2 a & b
    Fig 2 a & b, Construction of a genetic toggle switch in Escherichia coli, doi: 10.1038/35002131

---
+++ gardner2000/fig2a.m
@@ -0,0 +1,18 @@
+figure
+hold on
+
+alpha_2 = 9;
+gamma = 2;
+u = 0:0.01:10;
+v = alpha_2./(1 + u.^gamma);
+plot(u, v, 'k', 'LineWidth', 2)
+
+alpha_1 = 9;
+beta = 2.1;
+v = 0:0.01:10;
+u = alpha_1./(1 + v.^beta);
+plot(u, v, 'k', 'LineWidth', 2)
+
+xlabel('u')
+ylabel('v')
+title('A bistable toggle network with balanced promoter strengths.')
---
+++ gardner2000/fig2b.m
@@ -0,0 +1,18 @@
+figure
+hold on
+
+alpha_2 = 4;
+gamma = 1.4;
+u = 0:0.01:10;
+v = alpha_2./(1 + u.^gamma);
+plot(u, v, 'k', 'LineWidth', 2)
+
+alpha_1 = 9;
+beta = 2.1;
+v = 0:0.01:10;
+u = alpha_1./(1 + v.^beta);
+plot(u, v, 'k', 'LineWidth', 2)
+
+xlabel('u')
+ylabel('v')
+title('A monostable toggle network with imbalanced promoter strengths.')
